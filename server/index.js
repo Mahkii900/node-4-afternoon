48f5f6aa8683a5e9629c531b94983cf0ad5b2002
Mahkii900 <col.graff.battleschool@gmail.com>
2019-07-30 14:47:27 -0600

    finished step 7

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const session = require('express-session')
 const {PORT, SESSION_SECRET} = process.env
 const checkSession = require('./middlewares/checkForSession')
 const swagCtrl = require('./controllers/swagController')
+const authCtrl = require('./controllers/authController')
 
 //TOP LEVEL MIDDLEWARE
 app.use(express.json())
@@ -17,6 +18,10 @@ app.use(checkSession)
 
 //ENPOINTS
 app.get('/api/swag', swagCtrl.read)
+app.post('/api/login', authCtrl.login)
+app.post('/api/register', authCtrl.register)
+app.post('/api/signout', authCtrl.signout)
+app.get('/api/user', authCtrl.getUser)
 
 //LISTENER
 app.listen(PORT, () => console.log(`Captain's Log #${PORT}: We are lost in space...`))
